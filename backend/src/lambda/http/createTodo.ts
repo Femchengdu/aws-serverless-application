4248d42aab83c87964fe2ad854efcd8583630674
Samuel Ajayi <femiajayi705@gmail.com>
2022-05-29 10:28:34 +0200

    refac: Added validation for create todo without name

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -5,17 +5,28 @@ import { cors } from 'middy/middlewares'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 import { getUserId } from '../utils';
 import { createTodo } from '../../businessLogic/todos'
-
+import { createLogger } from '../../utils/logger'
+const logger = createLogger('CreateTodos')
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const newTodo: CreateTodoRequest = JSON.parse(event.body)
+    if (!newTodo.name) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: `The "name" property is required`
+        })
+      }
+    }
 
     const userId = getUserId(event)
-    await createTodo(newTodo, userId)
+    const newItemId = await createTodo(newTodo, userId)
+    logger.info('New item id?', { newItemId })
+
     return {
       statusCode: 201,
       body: JSON.stringify({
-        item: newTodo
+        item: { ...newTodo, todoId: newItemId }
       })
     }
   }
